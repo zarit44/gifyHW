1d6ec6d3ac46d0b0adc025950f3ddfbe9f928607
zarito <zarit.ortiz@outlook.com>
2019-10-08 23:42:28 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# gifyHW
+# giphyHomework
+
+## Giphy App!
+This app allows you to show you 10 gifs of your favorite actor! Simply press the button and the page will generate all the gifs you need. Before, my api was working but it was only giving my gifs that were the standard. I fixed my API and code to show the gifs that I actually want, not just the generic ones. I still am working on making my button to add new searches work, bugs will be fixed soon! In the mean time please enjoy the work that I do have done, I added extra buttons with some of my favorite actors so it looks a lot better. Enjoy! 
--- javaa.js
+++ javaa.js
@@ -1,23 +1,47 @@
+$(function(){
+    populateButtons(searchArray,'searchButton',"#buttonsArea");
+    console.log("Page loaded");
+})
 
-var buttonsArray = ["Emma Stone", "Margot Robbie"];
-for (var i = 0; i < buttonsArray.length; i++) {
-    var actorBtn = $("<button>");
-    actorBtn.addClass("actor-button actor actor-button-name");
-    actorBtn.attr("data-actor", buttonsArray[i]);
-    actorBtn.text(buttonsArray[i]);
-    $("#buttons").append(actorBtn);
+var searchArray = ['Emma Stone', 'Margot Robbie', 'Denzel Washington','Mahershala Ali', 'Tessa Thompson', 'Leonardo DiCaprio', 'Joaquin Phoenix', 'Madelaine Petch','Lili Reinhart'];
+
+function populateButtons(searchArray,classToAdd,areaToAddTo){
+    $(areaToAddTo).empty();
+    for(var i=0;i<searchArray.length;i++){
+        var a = $('<button>');
+        a.addClass(classToAdd);
+        a.attr('data-type', searchArray[i]);
+        a.text(searchArray[i]);
+        $(areaToAddTo).append(a);
+    }
 }
 
-$('button').on('click',function(){
-    var type= $(this).data("type");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=SED77J2m6eb60POCxbonYwCeBwSvLPmq&limit=10";
-    
-
-    $.ajax({url:queryURL, method: "GET"}).done(function(response){
-        
-        for(var i=0; i<response.data.length; i++){
-            $('#giphyPlace').prepend("<p>Rating: " +response.data[i].rating+"</p>");
-            $('#giphyPlace').prepend("<img src= '"+response.data[i].images.downsized.url+"'>");
+$(document).on('click','.searchButton',function(){
+    var type = $(this).data('type');
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q='+ type + '&api_key=SED77J2m6eb60POCxbonYwCeBwSvLPmq&limit=10';
+    $.ajax({url:queryURL, method:'GET'}).done(function(response){
+        for(var i=0;i<response.data.length;i++){
+            var searchDiv = $('<div class="search-item">');
+            var rating = response.data[i].rating;
+            var p = $('<p>').text('Rating:' + rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $('<img>');
+            image.attr('src',still);
+            image.attr('data-still',still);
+            image.attr('data-animated',animated);
+            image.attr('data-state','still');
+            image.addClass('searchImage');
+            searchDiv.append(p);
+            searchDiv.append(image);
+            $('#searches').append(searchDiv);
         }
     })
+})
+
+$('#addSearch').on('click',function(){
+    var newSearch = $('input').eq(0).val();
+    searchArray.push(newSearch);
+    populateButtons(searchArray,'searchButton', '#buttonsArea');
+    return false;
 })
